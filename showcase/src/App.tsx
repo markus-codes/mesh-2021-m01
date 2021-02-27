a8061ad7cd2000b53ee22f7278763bc5467c1813
Timo Schmidberger <schmidi.timo@gmail.com>
2021-02-27 17:38:10 +0100

    Added grid

--- showcase/src/App.tsx
+++ showcase/src/App.tsx
@@ -2,11 +2,14 @@ import './App.css';
 import { useEffect, useState } from 'react'
 import { VehicleInterface } from './types/base'
 import { useCreatWebSocket } from './hooks/useCreatWebSocket'
-import { Stage, Layer, Circle } from 'react-konva'
+import { Stage, Layer, Circle, Line } from 'react-konva'
 
 function App() {
   const [vehicle, setVehicle] = useState<VehicleInterface>()
   const [currentPoints, setCurrentPoints] = useState<VehicleInterface[]>([])
+
+  const width= 400 
+  const height= 400
 
   useCreatWebSocket(setVehicle)
 
@@ -27,7 +30,28 @@ function App() {
     <div className="App">
       <h1>Page One</h1>
       <div className={'container'}>
-        <Stage width={400} height={400}>
+        <Stage width={width} height={height}>
+          <Layer>
+            <Line points={[width / 10 * 1, 0, width / 10 * 1, height]} stroke={'grey'} strokeWidth={1} dash={[10, 10]} />
+            <Line points={[width / 10 * 2, 0, width / 10 * 2, height]} stroke={'grey'} strokeWidth={1} dash={[10, 10]} />
+            <Line points={[width / 10 * 3, 0, width / 10 * 3, height]} stroke={'grey'} strokeWidth={1} dash={[10, 10]} />
+            <Line points={[width / 10 * 4, 0, width / 10 * 4, height]} stroke={'grey'} strokeWidth={1} dash={[10, 10]} />
+            <Line points={[width / 10 * 5, 0, width / 10 * 5, height]} stroke={'grey'} strokeWidth={1} dash={[10, 10]} />
+            <Line points={[width / 10 * 6, 0, width / 10 * 6, height]} stroke={'grey'} strokeWidth={1} dash={[10, 10]} />
+            <Line points={[width / 10 * 7, 0, width / 10 * 7, height]} stroke={'grey'} strokeWidth={1} dash={[10, 10]} />
+            <Line points={[width / 10 * 8, 0, width / 10 * 8, height]} stroke={'grey'} strokeWidth={1} dash={[10, 10]} />
+            <Line points={[width / 10 * 9, 0, width / 10 * 9, height]} stroke={'grey'} strokeWidth={1} dash={[10, 10]} />
+
+            <Line points={[0, height / 10 * 1, width, height / 10 * 1]} stroke={'grey'} strokeWidth={1} dash={[10, 10]} />
+            <Line points={[0, height / 10 * 2, width, height / 10 * 2]} stroke={'grey'} strokeWidth={1} dash={[10, 10]} />
+            <Line points={[0, height / 10 * 3, width, height / 10 * 3]} stroke={'grey'} strokeWidth={1} dash={[10, 10]} />
+            <Line points={[0, height / 10 * 4, width, height / 10 * 4]} stroke={'grey'} strokeWidth={1} dash={[10, 10]} />
+            <Line points={[0, height / 10 * 5, width, height / 10 * 5]} stroke={'grey'} strokeWidth={1} dash={[10, 10]} />
+            <Line points={[0, height / 10 * 6, width, height / 10 * 6]} stroke={'grey'} strokeWidth={1} dash={[10, 10]} />
+            <Line points={[0, height / 10 * 7, width, height / 10 * 7]} stroke={'grey'} strokeWidth={1} dash={[10, 10]} />
+            <Line points={[0, height / 10 * 8, width, height / 10 * 8]} stroke={'grey'} strokeWidth={1} dash={[10, 10]} />
+            <Line points={[0, height / 10 * 9, width, height / 10 * 9]} stroke={'grey'} strokeWidth={1} dash={[10, 10]} />
+          </Layer>
           <Layer>
             {currentPoints.map((point, i) =>
               <Circle key={i} x={point.currentLocation.x * 4} y={point.currentLocation.y * 4} stroke="black" fill={point.isFine ? 'green' : 'red'} radius={2 * 4} />)
