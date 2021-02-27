f962eb3434483d01abe3822c16f7c59fa3634fde
Markus Heinemann <markus-heinemann@outlook.com>
2021-02-27 17:44:25 +0100

    Work on client simulator desing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,45 +1,62 @@
-import { useState } from 'react';
-import './App.css';
-import { generateData } from './generateData';
+import { useState } from "react";
+import "./App.css";
+import { generateData } from "./generateData";
 
 function App() {
-  const [numbInstances, setNumbInstances] = useState<number>(0)
-  const [instancesRunning, setInstancesRunning] = useState<boolean>(false)
-  const [instances, setInstances] = useState<WebSocket[]>([])
+  const [numbInstances, setNumbInstances] = useState<number>(0);
+  const [instancesRunning, setInstancesRunning] = useState<boolean>(false);
+  const [instances, setInstances] = useState<WebSocket[]>([]);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNumbInstances(parseInt(event.target.value));
-  }
+  };
 
   const handleSubmit = () => {
     if (!instancesRunning) {
-      setInstancesRunning(true)
-      setInstances(generateData(numbInstances))
+      setInstancesRunning(true);
+      setInstances(generateData(numbInstances));
     }
-  }
+  };
 
   return (
     <div className="app">
       <div className="container">
         <div className="card">
           <h1>Clients Simulator</h1>
-          <p>This application can simulate a specific number of clients. Each client represents a vehicle. How many items do you want to start?</p>
-          <div className="form-group">
-            <input type="number" name="name" autoComplete="off" className="form-control" min="0" max="5" onChange={(event) => handleChange(event)} />
-          </div>
-          <button type="button" className='button' onClick={() => handleSubmit()}>
-            Start Simulation
-          </button>
+          <p>
+            This application can simulate a specific number of clients. Each
+            client represents a vehicle.{" "}
+            <b>How many instances do you want to start?</b> Minimum is 1 and
+            maximum is 256.
+          </p>
 
-          { instancesRunning && 
+          {instancesRunning ? (
             <p>
               Instances running:
-              <span className="count">
-                {instances.length}
-              </span>
+              <span className="count">{instances.length}</span>
             </p>
-          }
-
+          ) : (
+            <>
+              <div className="form-group">
+                <input
+                  type="number"
+                  name="name"
+                  autoComplete="off"
+                  className="form-control"
+                  min="1"
+                  max="256"
+                  onChange={(event) => handleChange(event)}
+                />
+              </div>
+              <button
+                type="button"
+                className="button"
+                onClick={() => handleSubmit()}
+              >
+                Start Simulation
+              </button>
+            </>
+          )}
         </div>
       </div>
     </div>
